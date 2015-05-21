a0288b59839f30a5ffd7debdc571d98277df3b5f
peel3r <p@weljakubowski.com>
2015-05-21 17:46:29 +0100

    add signup page with sinatra gem

---
+++ lib/api/app/app.rb
@@ -0,0 +1,20 @@
+class App < Sinatra::Base
+  get "/"  do
+    @users = User.all
+    haml :index
+
+  end
+
+  get "/users/new" do
+    haml :signup
+  end
+
+  post "/users" do
+    service = Api::Services::Signup.new email: params[:user][:email], password: params[:user][:password]
+    service.call
+
+    if service.status == :ok
+      redirect "/"
+    end
+  end
+end
---
+++ lib/api/app/config.ru
@@ -0,0 +1,11 @@
+require 'bundler'
+Bundler.require
+require './app'
+
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: File.expand_path("../../../../db/development.sqlite3", __FILE__)
+)
+
+run App
